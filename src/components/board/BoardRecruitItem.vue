6d37751fb16e8d2ddd272b96faa95937a4147a38
김노아 <noah0504@naver.com>
2024-07-22 21:29:14 +0900

    FE-FM-S11P12A606-42 (1)

---
+++ src/components/board/BoardRecruitItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>       
+    <b-card
+      title="Team Name"
+      img-src="https://picsum.photos/600/300/?image=25"   
+      img-alt="Image"
+      img-top
+      style="max-width: 20rem;"
+      class="mb-2"
+    > <!-- img-src: team img url -->
+      <b-card-text>        
+        team introduction <br/>
+        project domain <br/>                 
+        teammate status
+      </b-card-text>
+
+      <div class="d-flex justify-content-end">
+        <b-button href="#" variant="primary">View</b-button>      
+      </div>
+    </b-card>
+  </div>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
 import App from './App.vue'
+import { BootstrapVue3 } from "bootstrap-vue-3";
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(BootstrapVue3)
+app.mount("#app");
