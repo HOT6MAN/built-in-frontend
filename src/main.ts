19fd76e2640903c7ed29fdf0e7033ff780c03c94
김노아 <noah0504@naver.com>
2024-07-22 21:57:59 +0900

    Feature/S11P12A606-42 (3)

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from './App.vue'
 import { BootstrapVue3 } from "bootstrap-vue-3";
 
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 const app = createApp(App);
 app.use(BootstrapVue3)
