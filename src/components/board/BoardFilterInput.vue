b3755aa6dca98bcb98843aa7f4ab753c6b055468
김노아 <noah0504@naver.com>
2024-07-22 22:07:47 +0900

    FE-FM-S11P12A606-42 (4)

---
+++ src/components/board/BoardFilterInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <b-input-group class="mb-3">            
+      <b-form-input></b-form-input>
+      <b-input-group-append>
+        <b-button text="submit" variant="primary">submit</b-button>
+      </b-input-group-append>
+    </b-input-group>    
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  
+</style>
