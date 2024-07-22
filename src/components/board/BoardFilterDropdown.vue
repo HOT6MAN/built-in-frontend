4cb9df2e13e5992aaaace12907577f41963ce1ed
김노아 <noah0504@naver.com>
2024-07-22 21:55:33 +0900

    FE-FM-S11P12A606-42 (2)

---
+++ src/components/board/BoardFilterDropdown.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-form-select :options="options" class="mb-3">
+      <template #first>
+        <b-form-select-option disabled> -- 검색 조건 -- </b-form-select-option>
+      </template>
+    </b-form-select>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const options = ref([
+  { text: "팀 이름" },
+  { text: "글쓴이" },
+  { text: "포지션" }
+]);
+</script>
+
+<style scoped>
+  
+</style>
