318a34a92ca77b3dd61c6cb1d1b7ff0305ceab63
김노아 <noah0504@naver.com>
2024-07-25 16:41:34 +0900

    FE-FM-S11P12A606-150 (2)

--- src/views/board/TeamBuildingRecruitView.vue
+++ src/views/board/TeamBuildingRecruitView.vue
@@ -33,39 +33,29 @@
   </div>
 </template>
 <script setup>
-  import {ref, computed} from 'vue'
+  import {ref, computed, defineProps} from 'vue'
   import FilterDropdown from '../../components/board/BoardFilterDropdown.vue';
   import FilterInput from '../../components/board/BoardFilterInput.vue'
   import RecruitItem from '../../components/board/BoardRecruitItem.vue'
 
+  const props = defineProps({
+    data: {
+      type: Array      
+    }
+  })
+
+  const { data } = props;
+
   const curPage = ref(1)
   const perPage = ref(6)
-
-  // TODO: request data
-  // dummy data 
-  const data = ref([
-    { id: 1, teamName: "ssafy", imgUrl: "https://picsum.photos/600/300/?image=25", introduction: "hello", domain: "ai", desiredPositions: ["FE", "BE", "ML"], author: "ssafy kim"},
-    { id: 2, teamName: "ssafy", imgUrl: "https://picsum.photos/600/300/?image=25", introduction: "hello", domain: "ai", desiredPositions: ["FE", "BE", "ML"], author: "ssafy kim"},
-    { id: 3, teamName: "ssafy", imgUrl: "https://picsum.photos/600/300/?image=25", introduction: "hello", domain: "ai", desiredPositions: ["FE", "BE", "ML"], author: "ssafy kim"},
-    { id: 4, teamName: "ssafy", imgUrl: "https://picsum.photos/600/300/?image=25", introduction: "hello", domain: "ai", desiredPositions: ["FE", "BE", "ML"], author: "ssafy kim"},
-    { id: 5, teamName: "ssafy", imgUrl: "https://picsum.photos/600/300/?image=25", introduction: "hello", domain: "ai", desiredPositions: ["FE", "BE", "ML"], author: "ssafy kim"},
-    { id: 6, teamName: "ssafy", imgUrl: "https://picsum.photos/600/300/?image=25", introduction: "hello", domain: "ai", desiredPositions: ["FE", "BE", "ML"], author: "ssafy kim"},
-    { id: 7, teamName: "ssafy", imgUrl: "https://picsum.photos/600/300/?image=25", introduction: "hello", domain: "ai", desiredPositions: ["FE", "BE", "ML"], author: "ssafy kim"},
-    { id: 8, teamName: "ssafy", imgUrl: "https://picsum.photos/600/300/?image=25", introduction: "hello", domain: "ai", desiredPositions: ["FE", "BE", "ML"], author: "ssafy kim"},
-    { id: 9, teamName: "ssafy", imgUrl: "https://picsum.photos/600/300/?image=25", introduction: "hello", domain: "ai", desiredPositions: ["FE", "BE", "ML"], author: "ssafy kim"},
-    { id: 10, teamName: "ssafy", imgUrl: "https://picsum.photos/600/300/?image=25", introduction: "hello", domain: "ai", desiredPositions: ["FE", "BE", "ML"], author: "ssafy kim"},
-    { id: 11, teamName: "ssafy", imgUrl: "https://picsum.photos/600/300/?image=25", introduction: "hello", domain: "ai", desiredPositions: ["FE", "BE", "ML"], author: "ssafy kim"},
-    { id: 12, teamName: "ssafy", imgUrl: "https://picsum.photos/600/300/?image=25", introduction: "hello", domain: "ai", desiredPositions: ["FE", "BE", "ML"], author: "ssafy kim"},
-    { id: 13, teamName: "ssafy", imgUrl: "https://picsum.photos/600/300/?image=25", introduction: "hello", domain: "ai", desiredPositions: ["FE", "BE", "ML"], author: "ssafy kim"},
-  ])
 
   const list = computed(() => {
     const start = (curPage.value - 1) * perPage.value;
     const end = curPage.value * perPage.value;
-    return data.value.slice(start, end)
+    return data.slice(start, end)
   })
 
-  const totRows = computed(() => data.value.length)
+  const totRows = computed(() => data.length)
 </script>
 <style scoped>
   .itemList {
--- src/views/board/TeamBuildingView.vue
+++ src/views/board/TeamBuildingView.vue
@@ -2,7 +2,7 @@
   <div>
     <b-tabs content-class="mt-3" pills align="center" class="teambuilding-tabbar">
       <b-tab title="Recruit" active>
-        <RecruitView />
+        <RecruitView :data/>
       </b-tab>
       <b-tab title="Self-Introduction">
         
@@ -11,7 +11,12 @@
   </div>
 </template>
 <script setup>
+  import { ref } from 'vue';
   import RecruitView from './TeamBuildingRecruitView.vue'
+
+  const data = ref([
+    { id: 1, teamName: "ssafy", imgUrl: "https://picsum.photos/600/300/?image=25", introduction: "hello", domain: "ai", desiredPositions: ["FE", "BE", "ML"], author: "ssafy kim"},
+  ])
 </script>
 <style scoped>
   .teambuilding-tabbar {
