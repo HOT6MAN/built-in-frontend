336d306cd2273d56745774915247a41cc6377b13
minho2633 <minho2633@naver.com>
2024-08-07 14:23:14 +0900

    style : 네비게이션 바 스타일 변경

--- src/views/Bars/NavigationBarView.vue
+++ src/views/Bars/NavigationBarView.vue
@@ -1,32 +1,31 @@
 <template>
   <div class="navigationBar">
-	<div class="sitelogo">
-	  <img class="logoIcon" alt="로고" src="@/icons/Navbar/Logo.svg" />
-	  <div class="logoText">Built-In</div>
-	</div>
-	<div class="menu">
-	  <RouterLink :to="{name : 'home'}" class="menuButton">홈</RouterLink>
-	  <RouterLink :to="{name: 'teambuilding'}" class="menuButton">팀빌딩</RouterLink>
-    <RouterLink :to="{name: 'dashboard'}" class="menuButton">프로젝트 관리</RouterLink>
-    <RouterLink :to="{name: 'resumes'}" class="menuButton">이력서</RouterLink>
-	</div>
-	<img class="alarmicon" alt="알림" src="@/icons/Navbar/alarm.svg" />
-	  <div v-if="authStore.isLogin">
-      	<div class="login" @click="clickLogout">
-      	<div class="profile">로그아웃</div>
-      	</div>
+      <RouterLink :to="{name: 'home'}" class="logo">
+        <img class="logoIcon" alt="로고" src="@/icons/Navbar/Logo.svg" />
+        <div class="logoText">Built-In</div>
+      </RouterLink>
+      <div class="menu">
+        <RouterLink :to="{name: 'teambuilding'}" class="menuButton">팀빌딩</RouterLink>
+        <RouterLink :to="{name: 'dashboard'}" class="menuButton">프로젝트 관리</RouterLink>
+        <RouterLink :to="{name: 'resumes'}" class="menuButton">이력서</RouterLink>
       </div>
-    <div v-else>
-      <div class="login" @click="toggleLoginModal()">
-          <div class="profile">로그인</div>
+    <div class="rightMenu">
+      <img class="alarmicon" alt="알림" src="@/icons/Navbar/alarm.svg" />
+      <div v-if="authStore.isLogin">
+          <div class="login" @click="clickLogout">
+          <div class="profile">로그아웃</div>
+          </div>
       </div>
-    </div>
-    <div>
+      <div v-else>
+        <b-button class="login" @click="toggleLoginModal()">
+            <div class="profile">로그인</div>
+        </b-button>
+      </div>
       <div @click="routeToMemberProfile">
-        <b-button><RouterLink :to="{name:'member'}">마이페이지</RouterLink></b-button>
+        <b-button variant="secondary"><RouterLink :to="{name:'member'}">마이페이지</RouterLink></b-button>
       </div>
     </div>
-    </div>
+  </div>
   <LoginModal :showLoginModal="showLoginModal" :toggleLoginModal="toggleLoginModal"/>
 </template>
 
@@ -66,10 +65,25 @@
 
 <style scoped>
 	/* 현재 각 요소들이 top, left를 이용해 위치를 잡고 있음. 코드를 쉽게 만들기 위해 div로 분류된 아이템들을 align-items를 통해 해결해야 함 */
-.sitelogo {
+.navigationBar{
+  position: fixed;
+  display : flex;
+  flex-direction : row;
+  align-items: center;
+  width: 100%;
+  height: 80px;
+  top: 0px;
+  text-align: left;
+  /* 아래 color와 font-family는 버튼 글자 색상과 폰트 스타일. 차후 버튼에 직접 넣을 것 */
+  font-family: var(--font-roboto);
+  background-color: #2d3648;
+  z-index: 10;
+}
+/* 로고 */
+.logo {
 	position: relative;
 	left: 9px;
-	width : auto;
+	width : 200px;
 	display: flex;
 	flex-direction: row;
 	align-items: center;
@@ -85,23 +99,34 @@
 	position: relative;
 	font-weight: 700;
 }
+/* 메뉴 */
 .menu {
-	position: absolute;
+	position: relative;
 	display: flex;
 	flex-direction: row;
-	gap: 40px;
-	position : relative;
-  left: 80px;
-  width: 80%;
+  width: auto;
+  gap: 40px;
+}
+/* a는 routerlink의 밑줄 없애는 용으로 만듦 */
+a {
+  text-decoration: none;
 }
 .menuButton {
-	color : white;
+	color : #EDF0F7;
 	font-size: var(--font-size-menu);
 	font-family: var(--font-roboto);
+}
 
+/* 메뉴 우측 */
+.rightMenu {
+  display: flex;
+  flex-direction: row;
+  position: relative;
+  margin-left: auto;
+  right: 30px;
+  gap : 15px;
 }
 .alarmicon {
-  position: relative;
 	right : 70px;
 	width : 40px;
 	height : 40px;
@@ -111,8 +136,6 @@
   display: flex;
   /* right: 37px; */
   border-radius: var(--radius-200);
-  background-color: rgba(255, 212, 58, 0.99);
-  border: 1px solid #ffedad;
   box-sizing: border-box;
   width: 101px;
   height: 40px;
@@ -120,28 +143,5 @@
   justify-content: center;
   padding: var(--space-300);
   gap: var(--space-200);
-	right : 30px;
 }
-.navigationBar{
-	width: 100%;
-  position: fixed;
-  height: 80px;
-  top: 0px;
-  overflow: hidden;
-  text-align: left;
-	/* 아래 color와 font-family는 버튼 글자 색상과 폰트 스타일. 차후 버튼에 직접 넣을 것 */
-  color: var(--color-buttons);
-  font-family: var(--font-roboto);
-	background-color: #2d3648;
-	display : flex;
-	flex-direction : row;
-	align-items : center;
-  z-index: 999;
-}
-/* .login:active {
-  margin-left:2px;
-  margin-top:2px;
-  box-shadow:none;
-} */
-
 </style>
