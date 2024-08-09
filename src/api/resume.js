0e191ca90e8cc6ea185f7905f16cccf71c46e8e3
김노아 <noah0504@naver.com>
2024-08-09 14:40:55 +0900

    FE-FM-S11P12A606-281-1 replace axios with api (jwt)

--- src/api/resume.js
+++ src/api/resume.js
@@ -1,32 +1,36 @@
-import { localAxios } from '@/util/http-commons'
-import api from '@/util/axios-common.js';
-const local = localAxios()
+import api from "@/util/axios-common";
+
+const local = api
 
 export function getImageFromUrl(url, successCB, failCB) {
-  api.get(url, {responseType: 'blob'}).then(successCB).catch(failCB)
+  local.get(url, {responseType: 'blob'}).then(successCB).catch(failCB)
 }
 
 export function findMyResumeList(successCB, failCB) {
-  api.get('/resumes').then(successCB).catch(failCB)
+  local.get('/resumes').then(successCB).catch(failCB)
 }
 
 export function registerResume(formData, successCB, failCB) {
-  api.post('/resume', formData, {headers: {'Content-Type': 'multipart/form-data'}})
+  local.post('/resume', formData, {headers: {'Content-Type': 'multipart/form-data'}})
     .then(successCB)
     .catch(failCB);
 }
 
 export async function findResumeById(id) {
-  const res = await api.get('/resume/' + id);
+  const res = await local.get('/resume/' + id);
   return res.data;
 }
 
 export function updateResume(id, formData, successCB, failCB) {
-  api.patch('/resume/' + id, formData, {headers: {'Content-Type': 'multipart/form-data'}})
+  local.patch('/resume/' + id, formData, {headers: {'Content-Type': 'multipart/form-data'}})
     .then(successCB)
     .catch(failCB);
 }
 
 export function deleteResumeById(id, successCB, failCB) {
-  api.delete('/resume/'+id).then(successCB).catch(failCB);
+  local.delete('/resume/'+id).then(successCB).catch(failCB);
+}
+
+export function applyTeamByResumeId(json, successCB, failCB) {
+  local.post('/apply', json).then(successCB).catch(failCB);
 }
--- src/api/teambuilding.js
+++ src/api/teambuilding.js
@@ -1,36 +1,35 @@
-import { localAxios } from '@/util/http-commons'
-import api from '@/util/axios-common.js';
-const local = localAxios()
+import api from "@/util/axios-common";
+
+const local = api
 
 export function findRecruitList(params, successCB, failCB) {
-  api.get('/teambuilding/recruits', { params }).then(successCB).catch(failCB)
+  local.get('/teambuilding/recruits', { params }).then(successCB).catch(failCB)
 }
 
 export async function findTeamList(params, successCB, failCB) {
-  console.log(" findTeamList before Send params = ", params);
-  await api.get('/team', { params }).then(successCB).catch(failCB)
+  await local.get('/team', { params }).then(successCB).catch(failCB)
 }
 
 export function registerRecruit(formData, successCB, failCB) {
-  api.post('/teambuilding/recruit', formData, {headers: {'Content-Type': 'multipart/form-data'}})
+  local.post('/teambuilding/recruit', formData, {headers: {'Content-Type': 'multipart/form-data'}})
     .then(successCB)
     .catch(failCB);
 }
 
 export async function findRecruit(id, successCB, failCB) {
-  await api.get('/teambuilding/recruit/' + id).then(successCB).catch(failCB)
+  await local.get('/teambuilding/recruit/' + id).then(successCB).catch(failCB)
 }
 
 export function deleteRecruit(id, successCB, failCB) {
-  api.delete('/teambuilding/recruit/'+ id).then(successCB).catch(failCB)
+  local.delete('/teambuilding/recruit/'+ id).then(successCB).catch(failCB)
 }
 
 export function getImageFromUrl(url, successCB, failCB) {
-  api.get(url, {responseType: 'blob'}).then(successCB).catch(failCB)
+  local.get(url, {responseType: 'blob'}).then(successCB).catch(failCB)
 }
 
 export function updateRecruit(id, formData, successCB, failCB) {
-  api.patch('/teambuilding/recruit/'+id, formData, {headers: {'Content-Type': 'multipart/form-data'}})
+  local.patch('/teambuilding/recruit/'+id, formData, {headers: {'Content-Type': 'multipart/form-data'}})
     .then(successCB)
     .catch(failCB);
 }
