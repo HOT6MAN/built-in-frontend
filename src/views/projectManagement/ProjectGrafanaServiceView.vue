3fbcb81b4b3d450fad9f93407bfff9eea59b681d
조용수 <soo_0201@naver.com>
2024-08-16 09:04:54 +0900

    grafana test

--- src/views/projectManagement/ProjectGrafanaServiceView.vue
+++ src/views/projectManagement/ProjectGrafanaServiceView.vue
@@ -1,28 +1,36 @@
 <template>
-    <div id="app">
-      <iframe ref="grafanaDashboard" width="100%" height="800" frameborder="0"></iframe>
-    </div>
-  </template>
+  <div id="app">
+    <iframe ref="grafanaDashboard" width="100%" height="800" frameborder="0"></iframe>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import { useProjectStore } from '../../stores/projectStore';
+import { storeToRefs } from 'pinia';
+
+const grafanaDashboard = ref(null);
+
+const projectStore = useProjectStore();
+const { serviceScheduleInfo } = storeToRefs(projectStore);
+
+onMounted(() => {
   
-  <script>
-  export default {
-    name: 'App',
-    mounted() {
-      const iframe = this.$refs.grafanaDashboard;
-      const dashboardURL = "https://grafana.hot6man.duckdns.org/d/mU7NJ3242g8Gk/cddba4f5-4e76-59e5-a8e7-6a053ee68c1a?orgId=1&refresh=5s";
-      const apiToken = 'Bearer glsa_4spgxgWayJiRV79eM28Lss3kUdRTUtEs_5b55abbf';
-      
-      iframe.src = dashboardURL;
-  
-      iframe.onload = function() {
-        iframe.contentWindow.postMessage({ message: 'set_token', token: apiToken }, '*');
-      };
-    }
+  const iframe = grafanaDashboard.value;
+  const dashboardURL = "https://grafana.hot6man.duckdns.org" + serviceScheduleInfo.value.grafanaUid + "?orgId=1&refresh=5s";
+  console.log("dashboardURL : ", dashboardURL);
+  const apiToken = 'Bearer glsa_4spgxgWayJiRV79eM28Lss3kUdRTUtEs_5b55abbf';
+
+  iframe.src = dashboardURL;
+
+  iframe.onload = function () {
+    iframe.contentWindow.postMessage({ message: 'set_token', token: apiToken }, '*');
   };
-  </script>
-  
-  <style>
-  #app {
-    text-align: center;
-  }
-  </style>
+});
+</script>
+
+<style>
+#app {
+  text-align: center;
+}
+</style>
