430df0e9af4aaf744a3b041460020fd4bd05f34c
이현재 <mystery0706@naver.com>
2024-08-16 09:33:14 +0900

    Fix: sonarQube rollback

--- src/views/projectManagement/ProjectSonarQubeServiceView.vue
+++ src/views/projectManagement/ProjectSonarQubeServiceView.vue
@@ -1,49 +1,27 @@
 <template>
   <div id="app">
-    <div>
-      {{ serviceScheduleInfo }}
-    </div>
-    <div v-if="serviceScheduleInfo.value.id">
-      <iframe ref="sonarQubeDashboard" width="100%" height="800" frameborder="0"></iframe>
-    </div>
-    <div v-else>
-      <hr />
-      <p style="font-size: 20px;">빌드된 프로젝트가 없습니다.</p>
-    </div>
+    <iframe ref="sonarQubeDashboard" width="100%" height="600" frameborder="0"></iframe>
   </div>
 </template>
 
-<script setup>
-import { ref, watch } from 'vue';
-import { useProjectStore } from '../../stores/projectStore';
-import { storeToRefs } from 'pinia';
+<script>
+export default {
+  name: 'App',
+  mounted() {
+    const iframe = this.$refs.sonarQubeDashboard;
+    const dashboardURL = "https://sonarqube.hot6man.duckdns.org/dashboard?id=sonarqube_4"; // 원복
+    const apiToken = 'Bearer sqa_62b2bc5a57c05a977ec55752d70a5a5b105c9d51';
+    
+    iframe.src = dashboardURL;
 
-const { serviceScheduleInfo } = storeToRefs(useProjectStore());
-const sonarQubeDashboard = ref(null);
-
-// Watcher to track changes in serviceScheduleInfo.value.id
-watch(
-  () => serviceScheduleInfo.value.id,
-  (newId) => {
-    if (newId) {
-      const dashboardURL = `https://sonarqube.hot6man.duckdns.org/dashboard?id=sonarqube_${newId}`;
-      const apiToken = 'Bearer sqa_62b2bc5a57c05a977ec55752d70a5a5b105c9d51';
-
-      sonarQubeDashboard.value.src = dashboardURL;
-
-      sonarQubeDashboard.value.onload = function () {
-        sonarQubeDashboard.value.contentWindow.postMessage(
-          { message: 'Authorization', token: apiToken },
-          '*'
-        );
-      };
-    }
-  },
-  { immediate: true }
-);
+    iframe.onload = function() {
+      iframe.contentWindow.postMessage({ message: 'Authorization', token: apiToken }, '*');
+    };
+  }
+};
 </script>
 
-<style scoped>
+<style>
 #app {
   text-align: center;
 }
